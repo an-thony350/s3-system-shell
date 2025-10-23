00c4f0c24640d850d1746bb99c9cd59454ca429d
derza-essien <d.erzaessien@outlook.com>
2025-10-23 12:29:34 +0100

    [Update]: task 3 functions

--- s3-system-shell/s3.c
+++ s3-system-shell/s3.c
@@ -3,19 +3,11 @@
 ///Simple for now, but will be expanded in a following section
 void construct_shell_prompt(char shell_prompt[])
 {
-    char cwd[MAX_PROMPT_LEN];
-
-    if(getcwd(cwd, sizeof(cwd)) != NULL){
-        snprintf(shell_prompt, MAX_PROMPT_LEN, "[%s]$ ", cwd);
-        //snprintf used to changed the memory contents in shell_prompt
-    }
-    else{
-        strcpy(shell_prompt, "[s3]$ ");
-    }
+    strcpy(shell_prompt, "[s3]$ ");
 }
 
 ///Prints a shell prompt and reads input from the user
-void read_command_line(char line[], char lwd[])
+void read_command_line(char line[], char lwd)
 {
     char shell_prompt[MAX_PROMPT_LEN];
     construct_shell_prompt(shell_prompt);
@@ -243,14 +235,6 @@ void launch_program_with_redirection(char *args[], int argsc){
     }
 }
 
-void init_lwd(char lwd[]){
-    if(getcwd(lwd, MAX_PROMPT_LEN) == NULL){ //
-        perror("getcwd failed");
-        strcpy(lwd, ".");
-        //Ensures that lwd stays in the current directory
-        // if it containts "." and not the cwd
-    }
-}
 
 int is_cd(char line[]) {
     if (line == NULL) return 0;
@@ -265,25 +249,4 @@ int is_cd(char line[]) {
     }
     
     return 0;
-}
-
-void run_cd(char *args[], int argsc, char lwd[]){
-    char cwd[MAX_PROMPT_LEN];
-    get(cwd, sizeof(cwd));
-
-    if(argsc == 1){
-        char *home = getenv("HOME");
-        if (home) chdir(home);
-        else perror("Home environment not set");
-    }
-    else if(argsc == 2){
-        if(strcmp(args[1], "-") == 0) chdir(lwd);
-        else chdir(args[1]);
-    }
-    else{
-        perror("Too many arguments");
-        return;
-    }
-
-    strcpy(lwd, cwd);
 }
